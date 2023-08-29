19cef848e03a8ac3bd359c96473a2b5d1434fa63
nilswende <nilswende@users.noreply.github.com>
2023-08-29 09:14:46 +0200

    test WordList

--- src/test/java/com/wn/nlp/jlani/impl/WordListTest.java
+++ src/test/java/com/wn/nlp/jlani/impl/WordListTest.java
@@ -5,6 +5,7 @@ import com.wn.nlp.jlani.value.Word;
 import org.junit.jupiter.api.Test;
 
 import java.io.StringReader;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,13 +15,22 @@ class WordListTest {
 	}
 	
 	@Test
-	void test() {
+	void testString() {
 		String wordlistStr = """
 				3f0601068209354a Rat
 				3efc28803f258cb7 schwierigen
 				3efe194a134042e1 Landgericht
 				""";
 		var wordList = getWordList(wordlistStr);
+		assertEquals(wordList.getLanguage(), new Language("de"));
+		assertTrue(wordList.getLikelihood(new Word("Rat")) > 0);
+		assertEquals(0, wordList.getLikelihood(new Word("Maus")));
+	}
+	
+	@Test
+	void testFile() {
+		var path = Path.of("./resources/wordlists/de.txt");
+		var wordList = InMemoryWordList.ofSerializedFile(path);
 		assertEquals(wordList.getLanguage(), new Language("de"));
 		assertTrue(wordList.getLikelihood(new Word("Rat")) > 0);
 		assertEquals(0, wordList.getLikelihood(new Word("Maus")));
