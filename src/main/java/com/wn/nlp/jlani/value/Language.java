586b76bd3acd07891da3ff47011eb9784637fb2c
nilswende <nilswende@users.noreply.github.com>
2023-09-10 10:05:15 +0200

    add doc

--- src/main/java/com/wn/nlp/jlani/value/Language.java
+++ src/main/java/com/wn/nlp/jlani/value/Language.java
@@ -11,15 +11,27 @@ import java.util.Objects;
 public record Language(String name) {
 	public Language {
 		Objects.requireNonNull(name);
-		if (name.isBlank()) throw new IllegalArgumentException("name must not be blank");
+		if (name.isBlank()) throw new IllegalArgumentException("name is blank");
 		name = name.toLowerCase();
 	}
 	
+	/**
+	 * Extracts the language name from the file name of a path.
+	 *
+	 * @param path the path
+	 * @return the language
+	 */
 	public static Language ofPath(final Path path) {
 		var fileName = path.getFileName().toString();
 		return ofFileName(fileName);
 	}
 	
+	/**
+	 * Extracts the language name from the file name.
+	 *
+	 * @param fileName the file name
+	 * @return the language
+	 */
 	public static Language ofFileName(final String fileName) {
 		var languageName = fileName.split("\\.")[0];
 		return new Language(languageName);
